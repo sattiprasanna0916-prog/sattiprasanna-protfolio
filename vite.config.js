83449cda2cd74aa622aefac8111571f6c726d11d
sattiprasanna0916-prog <sattiprasanna0916@gmail.com>
2026-08-17 19:42:48 +0530

    Fix portfolio GitHub Pages path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 export default defineConfig({
-  base: '/',
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: '/sattiprasanna-protfolio/',
 })
